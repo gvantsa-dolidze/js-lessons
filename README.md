0b9a7dcd357503752a3bb5991fd7dfc2910ddb27
gvantsa-dolidze <132009647+gvantsa-dolidze@users.noreply.github.com>
2024-08-27 22:27:38 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-lessons
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/gvantsa-dolidze/js-lessons)
---
+++ bitcamp/lib.js
@@ -0,0 +1,6 @@
+// Import readline-sync
+import readlineSync from 'readline-sync';
+
+// Use readline-sync to read input from the console
+export const input = readlineSync.question;
+export const print = console.log;
